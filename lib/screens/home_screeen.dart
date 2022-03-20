3ed64b6276d2eb40c23b8852333bc5b7a9123b41
davidngc <dsantamariaz@uni.pe>
2022-03-20 16:51:41 -0500

    Agregando Scafold con stalessWidget

--- lib/screens/home_screeen.dart
+++ lib/screens/home_screeen.dart
@@ -5,8 +5,30 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Hello World'),
+    const TextStyle fontSizeText = TextStyle(fontSize: 30);
+
+    int counter = 0;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Screen'),
+        elevation: 5,
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            const Text('Numero de Clicks', style: fontSizeText),
+            Text('$counter', style: fontSizeText),
+          ],
+          mainAxisAlignment: MainAxisAlignment.center,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            print('presionado');
+            counter++;
+          },
+          child: const Icon(Icons.add)),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
